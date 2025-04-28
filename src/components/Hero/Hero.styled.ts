7ebbd71dd1faa93604595ae880a0f0248a65672e
Tatiana Kashcha <tkashcha@gmail.com>
2025-04-28 12:48:26 +0300

    upd style

--- src/components/Hero/Hero.styled.ts
+++ src/components/Hero/Hero.styled.ts
@@ -1,45 +1,53 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import imgBgHero from "../../images/backgroundImg.jpg";
 
 export const Container = styled.div`
   padding-left: 20px;
   padding-right: 20px;
   margin-left: auto;
   margin-right: auto;
-  width: 700px;
+  max-width: 700px;
 `;
 
 export const Thumb = styled.div`
   display: flex;
   justify-content: center;
+  height: 920px;
+  background-image: url(${imgBgHero});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
 export const StyledLink = styled(NavLink)`
   text-decoration: none;
   padding: 10px;
   font-size: 18px;
-  color: rgb(126 79 15);
+  color: rgb(58 63 132);
   font-weight: bold;
-  border: 1px solid rgb(126 79 15);
+  border: 1px solid rgb(58 63 132);
   border-radius: 4px;
   min-width: 100px;
   text-align: center;
   cursor: pointer;
-  background-color: rgb(242 228 178);
+  background-color: rgb(231 219 213);
   &:hover {
-    background-color: rgb(126 79 15);
-    color: rgb(242 228 178);
+    background-color: rgb(58 63 132);
+    color: rgb(231 219 213);
   }
 `;
 
 export const List = styled.ul`
-  margin-top: 35%;
   display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 0 20px;
+
   list-style: none;
   li {
     font-weight: bold;
-    &:not(:last-child) {
-      margin-right: 20px;
-    }
   }
 `;
